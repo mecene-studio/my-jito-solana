9cb4fe3f188c22b2b4fd5497547ecdb3f4d88741
turcottep <32850285+turcottep@users.noreply.github.com>
2024-03-18 11:26:00 -0400

    d

--- ledger/src/my_new_entrypoint.rs
+++ ledger/src/my_new_entrypoint.rs
@@ -77,28 +77,46 @@ async fn listen_to_shredstream() -> io::Result<()> {
                 Shred::ShredCode(shred_code) => match shred_code {
                     ShredCode::Legacy(legacy_shred_code) => {
                         // Access fields of `legacy_shred_code` if needed
-                        println!("shred_common_header {:?}", legacy_shred_code.common_header);
-                        println!("shred_coding_header {:?}", legacy_shred_code.coding_header);
+                        // println!("shred_common_header {:?}", legacy_shred_code.common_header);
+                        // println!("shred_coding_header {:?}", legacy_shred_code.coding_header);
                     }
                     ShredCode::Merkle(merkle_shred_code) => {
                         // Access fields of `merkle_shred_code`
-                        println!("shred_common_header {:?}", merkle_shred_code.coding_header);
-                        println!("shred_coding_header {:?}", merkle_shred_code.coding_header);
+                        // println!("shred_common_header {:?}", merkle_shred_code.coding_header);
+                        // println!("shred_coding_header {:?}", merkle_shred_code.coding_header);
                         // println!("shred_payload len {:?}", merkle_shred_code.payload.len());
                     }
                 },
                 Shred::ShredData(shred_data) => match shred_data {
                     ShredData::Legacy(legacy_shred_data) => {
                         // Access fields of `legacy_shred_data` if needed
-                        println!("shred_common_header {:?}", legacy_shred_data.common_header);
-                        println!("shred_coding_header {:?}", legacy_shred_data.data_header);
-                        println!("shred_payload len {:?}", legacy_shred_data.payload.len());
+                        println!(
+                            "Legacy shred_common_header {:?}",
+                            legacy_shred_data.common_header
+                        );
+                        println!(
+                            "Legacy shred_coding_header {:?}",
+                            legacy_shred_data.data_header
+                        );
+                        println!(
+                            "Legacy shred_payload len {:?}",
+                            legacy_shred_data.payload.len()
+                        );
                     }
                     ShredData::Merkle(merkle_shred_data) => {
                         // Access fields of `merkle_shred_data`
-                        println!("shred_common_header {:?}", merkle_shred_data.common_header);
-                        println!("shred_coding_header {:?}", merkle_shred_data.data_header);
-                        println!("shred_payload len {:?}", merkle_shred_data.payload.len());
+                        println!(
+                            "Merkle shred_common_header {:?}",
+                            merkle_shred_data.common_header
+                        );
+                        println!(
+                            "Merkle shred_coding_header {:?}",
+                            merkle_shred_data.data_header
+                        );
+                        println!(
+                            "Merkle shred_payload len {:?}",
+                            merkle_shred_data.payload.len()
+                        );
                     }
                 },
             }
